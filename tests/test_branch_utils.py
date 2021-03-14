501324d453ee93fa6e969ebfcabe981724dc18d2
Dan Amzulescu <35110114+qualidan@users.noreply.github.com>
2021-03-14 16:30:55 -0500

    fixed tests

--- tests/test_branch_utils.py
+++ tests/test_branch_utils.py
@@ -31,24 +31,24 @@ class TestStashLogicFunctions(unittest.TestCase):
         self.assertTrue(uncommitted_branch_name.startswith(UNCOMMITTED_BRANCH_NAME))
 
     @patch.object(branch_utils, "create_remote_branch")
-    @patch.object(branch_utils, "commit_to_local_branch")
+    @patch.object(branch_utils, "commit_to_local_temp_branch")
     @patch.object(branch_utils, "preserve_uncommitted_code")
-    @patch.object(branch_utils, "create_local_branch")
+    @patch.object(branch_utils, "create_local_temp_branch")
     @patch.object(branch_utils, "stash_local_changes")
     def test_switch_to_temp_branch_cleanrepo(self, mock5, mock4, mock3, mock2, mock1):
         # Arrange:
         mock_repo = MagicMock()
         mock_repo.is_dirty = MagicMock(return_value=False)
-        mock_repo.untracked_files = False
+        mock_repo.untracked_files = True
         defined_branch_in_file = MagicMock()
         # Act:
         uncommitted_branch_name, flag = self.switch(mock_repo, defined_branch_in_file)
         # Assert:
         mock1.assert_called_once_with(mock_repo, uncommitted_branch_name)
         mock2.assert_called_once_with(mock_repo)
-        mock3.assert_not_called()
+        mock3.assert_called_once_with(mock_repo)
         mock4.assert_called_once_with(mock_repo, uncommitted_branch_name)
-        mock5.assert_not_called()
+        mock3.assert_called_once_with(mock_repo)
         self.assertTrue(uncommitted_branch_name.startswith(UNCOMMITTED_BRANCH_NAME))
 
     @patch.object(branch_utils, "checkout_remote_branch")
