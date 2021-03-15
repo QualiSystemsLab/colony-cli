89eed3fcbd252c9375b1fe38d30fb9a6c9200e6a
doppleware <roni.dover@gmail.com>
2021-03-14 21:25:45 -0700

    sorting imports

--- colony/commands/sb.py
+++ colony/commands/sb.py
@@ -4,11 +4,7 @@ import os
 import time
 import tabulate
 from docopt import DocoptExit
-from colony.branch_utils import (
-    figure_out_branches,
-    revert_from_temp_branch,
-    wait_and_then_delete_branch
-)
+from colony.branch_utils import figure_out_branches, revert_from_temp_branch, wait_and_then_delete_branch
 from colony.commands.base import BaseCommand
 from colony.constants import UNCOMMITTED_BRANCH_NAME
 from colony.sandboxes import SandboxesManager
