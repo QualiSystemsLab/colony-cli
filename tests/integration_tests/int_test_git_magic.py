d93a0b1ee39f55e98efe9d7571946d9c078b55ce
Dan Amzulescu <35110114+qualidan@users.noreply.github.com>
2021-03-17 08:11:19 -0500

    incremental progress

--- tests/integration_tests/int_test_git_magic.py
+++ tests/integration_tests/int_test_git_magic.py
@@ -1,12 +1,28 @@
+import os
+import shutil
 import unittest
+import logging
 from unittest.mock import MagicMock, patch
 
+logging.getLogger("git").setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
 
 class GitMagicTests(unittest.TestCase):
 
     def setUp(self) -> None:
         # setup testing env
         # -> copy base_repo to temp folder
+        try:
+            os.chdir(f"tests{os.sep}integration_tests")
+            # os.mkdir("test_repo_temp")
+            src = f"{os.getcwd()}{os.sep}test_repo_gold"
+            dst = f"{os.getcwd()}{os.sep}test_repo_temp"
+            shutil.copytree(src, dst)
+        except Exception as e:
+            logger.warning(f"Was not able to create create a temp local dir. Reason: {str(e)}")
+
+
+        print(os.getcwd())
         # -> do git init on temp folder
         # -> set working directory to temp folder
         # -> git remote => mock/patch => git pull, git push, remote sync test (mock here or on class level)
@@ -17,7 +33,7 @@ class GitMagicTests(unittest.TestCase):
         # delete temp folder
         pass
 
-    def test_blueprint_validate_when_uncommited_changes(self):
+    def test_blueprint_validate_when_uncommitted_changes(self):
 
         # manipulate files/git state to set up current test case
 
