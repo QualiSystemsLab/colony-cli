2e8817ee62d81b7914cf28e396d1800184a27f4b
D4N <dan.amzulescu@gmail.com>
2021-04-07 15:40:30 -0500

    lint >:(

--- tests/integration_tests/int_test_git_magic.py
+++ tests/integration_tests/int_test_git_magic.py
@@ -11,11 +11,12 @@ from git import Repo
 from colony import branch_utils, shell
 from colony.constants import UNCOMMITTED_BRANCH_NAME
 from tests.helpers.repo_utils import (
-    achieve_dirty_and_untracked_repo,
-    create_clean_repo,
-    readonly_handler,
-    add_untracked,
-    make_repo_dirty,
+     achieve_dirty_and_untracked_repo,
+     add_untracked,
+     create_clean_repo,
+     make_repo_dirty,
+     readonly_handler,
+
 )
 
 logging.getLogger("git").setLevel(logging.WARNING)
